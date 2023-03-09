076ae7bc6bdba62fa043c13a98119e12feccf88d
colineckert <colineckert10@gmail.com>
2023-03-08 20:48:28 -0800

    show file status

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -13,7 +13,7 @@ require('lualine').setup {
     lualine_b = { 'branch' },
     lualine_c = { {
       'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
+      file_status = false, -- displays file status (readonly status, modified status)
       path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
     } },
     lualine_x = {
