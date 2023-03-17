d5db9633c625f0470beba23f742526d09d46c39b
colineckert <colineckert10@gmail.com>
2023-03-16 20:22:41 -0700

    set transparent

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -45,7 +45,7 @@ require("tokyonight").setup({
   -- or leave it empty to use the default settings
   style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
-  transparent = false, -- Enable this to disable setting the background color
+  transparent = true, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
     -- Style to be applied to different syntax groups
