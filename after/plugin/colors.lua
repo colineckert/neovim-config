538d66e63a3ea98a76c7d46e644d306aba360d69
colineckert <colineckert10@gmail.com>
2023-05-17 21:09:47 -0700

    update gruvbox

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -112,7 +112,7 @@ require("gruvbox").setup({
   palette_overrides = {},
   overrides = {},
   dim_inactive = false,
-  transparent_mode = false,
+  transparent_mode = true,
 })
 
 require('rose-pine').setup({
