e6ccad587fcb2a9aeeeb37beaa4842bed64f2302
colineckert <colineckert10@gmail.com>
2023-10-14 20:18:26 -0700

    update colors

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -236,6 +236,7 @@ _G.packer_plugins = {
     url = "https://github.com/rose-pine/neovim"
   },
   ["telescope.nvim"] = {
+    config = { "\27LJ\2\n¾\1\0\0\6\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\b\0005\3\6\0005\4\3\0005\5\4\0=\5\5\4=\4\a\3=\3\t\2B\0\2\1K\0\1\0\15extensions\1\0\0\tundo\1\0\0\18layout_config\1\0\1\19preview_height\4š³æÌ\t™³¦ÿ\3\1\0\2\17side_by_side\2\20layout_strategy\rvertical\nsetup\14telescope\frequire\0" },
     loaded = true,
     path = "/Users/colineckert/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
@@ -273,6 +274,10 @@ time([[Defining packer_plugins]], false)
 time([[Config for todo-comments.nvim]], true)
 try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18todo-comments\frequire\0", "config", "todo-comments.nvim")
 time([[Config for todo-comments.nvim]], false)
+-- Config for: telescope.nvim
+time([[Config for telescope.nvim]], true)
+try_loadstring("\27LJ\2\n¾\1\0\0\6\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\b\0005\3\6\0005\4\3\0005\5\4\0=\5\5\4=\4\a\3=\3\t\2B\0\2\1K\0\1\0\15extensions\1\0\0\tundo\1\0\0\18layout_config\1\0\1\19preview_height\4š³æÌ\t™³¦ÿ\3\1\0\2\17side_by_side\2\20layout_strategy\rvertical\nsetup\14telescope\frequire\0", "config", "telescope.nvim")
+time([[Config for telescope.nvim]], false)
 -- Config for: nvim-autopairs
 time([[Config for nvim-autopairs]], true)
 try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
