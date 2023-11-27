85aa532d2d5251b7c9db856619cf6d76972bc085
colineckert <colineckert10@gmail.com>
2023-11-26 16:51:11 -0800

    add remaps for diffview and todo-comment

---
+++ after/plugin/diffview.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<leader>df', ':DiffviewFileHistory<CR>')
+vim.keymap.set('n', '<leader>dc', ':DiffviewClose<CR>')
---
+++ after/plugin/todo-comments.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<leader>st', ':TodoTelescope<CR>')
+vim.keymap.set('n', '<leader>tl', ':TodoLocList<CR>')
+
