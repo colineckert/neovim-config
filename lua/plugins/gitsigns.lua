b664b83381a250b6cb1577ca200947878c1ad9c8
colineckert <colineckert10@gmail.com>
2024-01-10 05:57:06 -0800

    fix lsp config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,13 +2,13 @@ return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
 		-- See `:help gitsigns.txt`
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-		},
+		-- signs = {
+		-- 	add = { text = "+" },
+		-- 	change = { text = "~" },
+		-- 	delete = { text = "_" },
+		-- 	topdelete = { text = "‾" },
+		-- 	changedelete = { text = "~" },
+		-- },
 		on_attach = function(bufnr)
 			local gs = package.loaded.gitsigns
 
