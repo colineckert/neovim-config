72f8e4a7542b5e341de9a962b28d98b83e5d5dca
colineckert <colineckert10@gmail.com>
2024-01-31 05:54:14 -0800

    change tokyonight theme

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,24 +1,26 @@
 return {
-  'nvim-telescope/telescope.nvim', version = '0.1.4',
-  dependencies = {
-    {'nvim-lua/plenary.nvim'},
-    {'debugloop/telescope-undo.nvim'},
-  },
-  config = function ()
-    local builtin = require('telescope.builtin')
-    require("telescope").load_extension("undo")
+	"nvim-telescope/telescope.nvim",
+	version = "0.1.4",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{ "debugloop/telescope-undo.nvim" },
+	},
+	config = function()
+		local builtin = require("telescope.builtin")
+		require("telescope").load_extension("undo")
 
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
-    vim.keymap.set('n', '<C-f>', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end)
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { noremap = true, silent = true })
-    vim.keymap.set("n", "<leader>su", "<cmd>Telescope undo<cr>")
-  end
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
+		-- vim.keymap.set('n', '<C-f>', builtin.git_files, {})
+		vim.keymap.set("n", "<leader>ps", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+		vim.keymap.set("n", "<leader>sr", builtin.resume, { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>su", "<cmd>Telescope undo<cr>")
+		vim.keymap.set("n", "<leader>sc", "<cmd>Telescope git_commits<cr>")
+	end,
 }
 
 -- REFERENCE
