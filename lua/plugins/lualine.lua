ba83d616daf1fb8b4af49075fe23eca0849bf96a
colineckert <colineckert10@gmail.com>
2024-03-03 18:30:37 -0800

    fix lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,47 +1,92 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  config = function()
-    local lualine = require('lualine')
-    lualine.setup {
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
-        -- component_separators = '|',
-        -- section_separators = '',
-        sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff' },
-        lualine_c = { {
-          'filename',
-          file_status = true, -- displays file status (readonly status, modified status)
-          path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
-        } },
-        lualine_x = {
-          { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
-            hint = ' ' } },
-          'encoding',
-          'filetype'
-        },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { {
-          'filename',
-          file_status = true, -- displays file status (readonly status, modified status)
-          path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
-        } },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
-      },
-      tabline = {},
-      extensions = { 'fugitive' }
-      },
-    }
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- Displays file status (readonly status, modified status)
+						newfile_status = false, -- Display new file status (new file means no write after created)
+						path = 4, -- 0: Just the filename
+						-- 1: Relative path
+						-- 2: Absolute path
+						-- 3: Absolute path, with tilde as the home directory
+						-- 4: Filename and parent dir, with tilde as the home directory
+
+						shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+						-- for other components. (terrible name, any suggestions?)
+						symbols = {
+							modified = "[+]", -- Text to show when the file is modified.
+							readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+							unnamed = "[No Name]", -- Text to show for unnamed buffers.
+							newfile = "[New]", -- Text to show for newly created file before first write
+						},
+					},
+				},
+				lualine_x = {
+					{
+						"diagnostics",
+
+						-- Table of diagnostic sources, available sources are:
+						--   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+						sources = { "nvim_diagnostic", "nvim_lsp", "vim_lsp" },
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
+
+						-- Displays diagnostics for the defined severity types
+						sections = { "error", "warn", "info", "hint" },
+
+						diagnostics_color = {
+							-- Same values as the general color option can be used here.
+							error = "DiagnosticError", -- Changes diagnostics' error color.
+							warn = "DiagnosticWarn", -- Changes diagnostics' warn color.
+							info = "DiagnosticInfo", -- Changes diagnostics' info color.
+							hint = "DiagnosticHint", -- Changes diagnostics' hint color.
+						},
+						colored = true, -- Displays diagnostics status in color if set to true.
+						update_in_insert = false, -- Update diagnostics in insert mode.
+						always_visible = false, -- Show diagnostics even if there are none.
+					},
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = { "fugitive" },
+		})
+	end,
 }
