6b1cfefb616a7d8461c9cf1dd7e266f5134e07e6
Colin Eckert <colineckert10@gmail.com>
2024-07-14 09:02:35 -0700

    update harpoon add

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,7 +10,7 @@ return {
 		harpoon:setup()
 
 		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():append()
+			harpoon:list():add()
 		end)
 		vim.keymap.set("n", "<C-e>", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
