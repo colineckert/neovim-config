0c64c3c7429152572b6222c110b4ee578a25811f
Colin Eckert <colineckert10@gmail.com>
2024-08-20 06:38:59 -0700

    update options and add keymaps

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,8 @@
--- Settings
+-- SETTINGS
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.mouse = "a"
 vim.opt.conceallevel = 1
 
 vim.opt.tabstop = 2
@@ -10,6 +11,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.breakindent = true
 
 vim.opt.wrap = false
 
@@ -19,7 +21,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.o.cursorline = true
-
+vim.opt.colorcolumn = "80"
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
@@ -28,20 +30,32 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
-
--- Remaps
+-- REMAPS
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Center buffer while navigating
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "{", "{zz")
+vim.keymap.set("n", "}", "}zz")
+vim.keymap.set("n", "G", "Gzz")
+vim.keymap.set("n", "gg", "ggzz")
+vim.keymap.set("n", "gd", "gdzz")
+vim.keymap.set("n", "<C-i>", "<C-i>zz")
+vim.keymap.set("n", "<C-o>", "<C-o>zz")
+vim.keymap.set("n", "%", "%zz")
+vim.keymap.set("n", "*", "*zz")
+vim.keymap.set("n", "#", "#zz")
+
+vim.keymap.set("n", "L", "$")
+vim.keymap.set("n", "H", "^")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
@@ -67,3 +81,9 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Press leader rw to rotate open windows
+vim.keymap.set("n", "<leader>rw", ":RotateWindows<cr>", { desc = "[R]otate [W]indows" })
+
+-- Press gx to open the link under the cursor
+vim.keymap.set("n", "gx", ":sil !open <cWORD><cr>", { silent = true })
