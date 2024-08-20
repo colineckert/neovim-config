d0dbc04c9db3b3a24e2f7a37295c9e504a666f30
Colin Eckert <colineckert10@gmail.com>
2024-08-20 06:40:57 -0700

    update oil keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,9 +5,23 @@ return {
 		config = function()
 			require("oil").setup({
 				columns = { "icon" },
+				use_default_keymaps = false,
 				keymaps = {
-					["<C-h>"] = false,
-					["<M-h>"] = "actions.select_split",
+					["g?"] = "actions.show_help",
+					["<CR>"] = "actions.select",
+					["<C-\\>"] = "actions.select_split",
+					["<C-enter>"] = "actions.select_vsplit", -- this is used to navigate left
+					["<C-t>"] = "actions.select_tab",
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = "actions.close",
+					["<C-r>"] = "actions.refresh",
+					["-"] = "actions.parent",
+					["_"] = "actions.open_cwd",
+					["`"] = "actions.cd",
+					["~"] = "actions.tcd",
+					["gs"] = "actions.change_sort",
+					["gx"] = "actions.open_external",
+					["g."] = "actions.toggle_hidden",
 				},
 				view_options = {
 					show_hidden = true,
