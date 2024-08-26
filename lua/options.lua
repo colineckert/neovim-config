b4037a0fdb5e0f3a4e18e6f0512d12d4e648752d
Colin Eckert <colineckert10@gmail.com>
2024-08-26 06:21:15 -0700

    add avante plugin

--- lua/options.lua
+++ lua/options.lua
@@ -29,6 +29,12 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
+-- views can only be fully collapsed with the global statusline
+vim.opt.laststatus = 3
+-- Default splitting will cause your main splits to jump when opening an edgebar.
+-- To prevent this, set `splitkeep` to either `screen` or `topline`.
+vim.opt.splitkeep = "screen"
+
 -- REMAPS
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
