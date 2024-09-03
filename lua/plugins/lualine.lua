dea88e94688961faae3052ff93e2de3f0a53646b
Colin Eckert <colineckert10@gmail.com>
2024-09-03 08:23:59 -0700

    Add and customize rose-pine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,6 +10,7 @@ return {
 				-- section_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				component_separators = { left = "", right = "" },
+				-- component_separators = { left = " ▎", right = " ▎" },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
