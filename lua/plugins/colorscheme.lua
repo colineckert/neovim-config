322ad750ee496efc3b2c78c0474fd96c0b94d86f
Colin Eckert <colineckert10@gmail.com>
2024-09-11 16:52:41 -0700

    Update italics

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -24,7 +24,7 @@ return {
 				no_underline = false, -- Force no underline
 				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
 					comments = { "italic" }, -- Change the style of comments
-					conditionals = { "italic" },
+					conditionals = {},
 					loops = {},
 					functions = {},
 					keywords = { "italic" },
