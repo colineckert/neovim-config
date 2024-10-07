a54b6f4934701b48e69721f8dc03289ce23cf1d4
Colin Eckert <colineckert10@gmail.com>
2024-10-07 09:56:10 -0700

    clean up colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,21 +22,21 @@ return {
 				no_italic = false, -- Force no italic
 				no_bold = false, -- Force no bold
 				no_underline = false, -- Force no underline
-				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-					comments = { "italic" }, -- Change the style of comments
-					conditionals = {},
-					loops = {},
-					functions = {},
-					keywords = { "italic" },
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = {},
-					properties = {},
-					types = {},
-					operators = {},
-					-- miscs = {}, -- Uncomment to turn off hard-coded styles
-				},
+				-- styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				-- 	comments = { "italic" }, -- Change the style of comments
+				-- 	conditionals = { "italic" },
+				-- 	loops = {},
+				-- 	functions = { "italic" },
+				-- 	keywords = {},
+				-- 	strings = {},
+				-- 	variables = {},
+				-- 	numbers = {},
+				-- 	booleans = {},
+				-- 	properties = {},
+				-- 	types = {},
+				-- 	operators = {},
+				-- miscs = {}, -- Uncomment to turn off hard-coded styles
+				-- },
 				color_overrides = {},
 				custom_highlights = {},
 				default_integrations = true,
@@ -63,151 +63,5 @@ return {
 				},
 			})
 		end,
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("rose-pine").setup({
-				variant = "auto", -- auto, main, moon, or dawn
-				dark_variant = "main", -- main, moon, or dawn
-				dim_inactive_windows = false,
-				extend_background_behind_borders = true,
-
-				enable = {
-					terminal = true,
-					legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-					migrations = true, -- Handle deprecated options automatically
-				},
-
-				styles = {
-					bold = true,
-					italic = false,
-					transparency = false,
-				},
-
-				groups = {
-					border = "muted",
-					link = "iris",
-					panel = "surface",
-
-					error = "love",
-					hint = "iris",
-					info = "foam",
-					note = "pine",
-					todo = "rose",
-					warn = "gold",
-
-					git_add = "foam",
-					git_change = "rose",
-					git_delete = "love",
-					git_dirty = "rose",
-					git_ignore = "muted",
-					git_merge = "iris",
-					git_rename = "pine",
-					git_stage = "iris",
-					git_text = "rose",
-					git_untracked = "subtle",
-
-					h1 = "iris",
-					h2 = "foam",
-					h3 = "rose",
-					h4 = "gold",
-					h5 = "pine",
-					h6 = "foam",
-				},
-
-				palette = {
-					-- Override the builtin palette per variant
-					-- moon = {
-					--     base = '#18191a',
-					--     overlay = '#363738',
-					-- },
-				},
-
-				highlight_groups = {
-					Comment = { italic = true },
-					["@keyword"] = { italic = true },
-					["@keyword.return"] = { italic = true },
-					["@conditional"] = { italic = true },
-					["@keyword.conditional"] = { italic = true },
-					["@lsp.type.keyword"] = { italic = true },
-					["@lsp.type.modifier"] = { italic = true },
-					-- ["@function"] = { italic = true },
-					-- ["@function.builtin"] = { italic = true },
-					-- ["@function.macro"] = { italic = true },
-					-- ["@function.method"] = { italic = true },
-					-- ["@function.method.call"] = { italic = true },
-					-- ["@parameter"] = { italic = true },
-					-- VertSplit = { fg = "muted", bg = "muted" },
-				},
-
-				before_highlight = function(group, highlight, palette)
-					-- Disable all undercurls
-					-- if highlight.undercurl then
-					--     highlight.undercurl = false
-					-- end
-					--
-					-- Change palette colour
-					-- if highlight.fg == palette.pine then
-					--     highlight.fg = palette.foam
-					-- end
-				end,
-			})
-		end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-			light_style = "day", -- The theme is used when the background is set to light
-			transparent = false, -- Enable this to disable setting the background color
-			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-			styles = {
-				-- Style to be applied to different syntax groups
-				-- Value is any valid attr-list value for `:help nvim_set_hl`
-				comments = { italic = true },
-				keywords = { italic = true },
-				functions = {},
-				variables = {},
-				-- Background styles. Can be "dark", "transparent" or "normal"
-				sidebars = "dark", -- style for sidebars, see below
-				floats = "dark", -- style for floating windows
-			},
-			day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-			dim_inactive = false, -- dims inactive windows
-			lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
-
-			--- You can override specific color groups to use other groups or a hex color
-			--- function will be called with a ColorScheme table
-			---@param colors ColorScheme
-			on_colors = function(colors) end,
-
-			--- You can override specific highlights to use other groups or a hex color
-			--- function will be called with a Highlights and ColorScheme table
-			---@param highlights tokyonight.Highlights
-			---@param colors ColorScheme
-			on_highlights = function(highlights, colors) end,
-
-			cache = true, -- When set to true, the theme will be cached for better performance
-
-			---@type table<string, boolean|{enabled:boolean}>
-			plugins = {
-				-- enable all plugins when not using lazy.nvim
-				-- set to false to manually enable/disable plugins
-				all = package.loaded.lazy == nil,
-				-- uses your plugin manager to automatically enable needed plugins
-				-- currently only lazy.nvim is supported
-				auto = true,
-				-- add any plugins here that you want to enable
-				-- for all possible plugins, see:
-				--   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
-				-- telescope = true,
-			},
-		},
 	},
 }
