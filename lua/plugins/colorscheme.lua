b8b991edef8c713f1f81e0ab3e390846dd714157
Colin Eckert <colineckert10@gmail.com>
2024-11-13 20:33:14 -0800

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,36 @@
 return {
 	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				variant = "auto", -- auto, main, moon, or dawn
+				dark_variant = "main", -- main, moon, or dawn
+				dim_inactive_windows = false,
+				extend_background_behind_borders = true,
+
+				enable = {
+					terminal = true,
+					legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+					migrations = true, -- Handle deprecated options automatically
+				},
+
+				styles = {
+					bold = true,
+					italic = false,
+					transparency = false,
+				},
+
+				highlight_groups = {
+					Comment = { italic = true },
+					["@keyword.conditional"] = { italic = true },
+				},
+			})
+		end,
+	},
+	{
 		"catppuccin/nvim",
 		lazy = false,
 		name = "catppuccin",
