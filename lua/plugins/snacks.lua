c4b026a5fdc6b14193d1466d28dfce8f96abbc76
Colin Eckert <colineckert10@gmail.com>
2025-06-30 06:05:43 -0700

    Revert colorscheme

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,147 +1,151 @@
--- local filtered_message = { "No information available" }
+local mocha = require("catppuccin.palettes").get_palette("mocha")
+
+vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = mocha.sapphire })
+
+local filtered_message = { "No information available" }
 
 return {
-	-- {
-	-- 	"folke/snacks.nvim",
-	-- 	priority = 1000,
-	-- 	lazy = false,
-	-- 	---@type snacks.Config
-	-- 	opts = {
-	-- 		bigfile = { enabled = true },
-	-- 		dim = { enabled = true },
-	-- 		git = { enabled = true },
-	-- 		gitbrowse = { enabled = true },
-	-- 		indent = { enabled = true },
-	-- 		notifier = {
-	-- 			enabled = true,
-	-- 			timeout = 3000,
-	-- 		},
-	-- 		quickfile = { enabled = true },
-	-- 		statuscolumn = { enabled = true },
-	-- 		words = { enabled = true },
-	-- 		zen = { enabled = true },
-	-- 	},
-	-- 	init = function()
-	-- 		vim.api.nvim_create_autocmd("User", {
-	-- 			pattern = "VeryLazy",
-	-- 			callback = function()
-	-- 				local notify = Snacks.notifier.notify
-	-- 				Snacks.notifier.notify = function(message, level, opts)
-	-- 					for _, msg in ipairs(filtered_message) do
-	-- 						if message == msg then
-	-- 							return nil
-	-- 						end
-	-- 					end
-	-- 					return notify(message, level, opts)
-	-- 				end
-	-- 			end,
-	-- 		})
-	-- 	end,
-	-- 	keys = {
-	-- 		{
-	-- 			"<leader>.",
-	-- 			function()
-	-- 				Snacks.scratch()
-	-- 			end,
-	-- 			desc = "Toggle Scratch Buffer",
-	-- 		},
-	-- 		{
-	-- 			"<leader>B",
-	-- 			function()
-	-- 				Snacks.scratch.select()
-	-- 			end,
-	-- 			desc = "Select Scratch [B]uffer",
-	-- 		},
-	-- 		{
-	-- 			"<leader>bd",
-	-- 			function()
-	-- 				Snacks.bufdelete()
-	-- 			end,
-	-- 			desc = "[B]uffer [D]elete",
-	-- 		},
-	-- 		{
-	-- 			"<leader>gb",
-	-- 			function()
-	-- 				Snacks.git.blame_line()
-	-- 			end,
-	-- 			desc = "[G]it [B]lame Line",
-	-- 		},
-	-- 		{
-	-- 			"<leader>og",
-	-- 			function()
-	-- 				Snacks.gitbrowse()
-	-- 			end,
-	-- 			desc = "[O]pen [G]it",
-	-- 			mode = { "n", "v" },
-	-- 		},
-	-- 		{
-	-- 			"<leader>nh",
-	-- 			function()
-	-- 				Snacks.notifier.show_history()
-	-- 			end,
-	-- 			desc = "Notification History",
-	-- 		},
-	-- 		{
-	-- 			"<leader>dn",
-	-- 			function()
-	-- 				Snacks.notifier.hide()
-	-- 			end,
-	-- 			desc = "[D]ismiss All [N]otifications",
-	-- 		},
-	-- 		{
-	-- 			"<leader>nh",
-	-- 			function()
-	-- 				Snacks.notifier.show_history()
-	-- 			end,
-	-- 			desc = "[N]otification [H]istory",
-	-- 		},
-	-- 		{
-	-- 			"<leader>z",
-	-- 			function()
-	-- 				Snacks.toggle.dim():toggle()
-	-- 			end,
-	-- 			desc = "Toggle [Z]en Mode",
-	-- 		},
-	--
-	-- 		{
-	-- 			"<leader>ln",
-	-- 			function()
-	-- 				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):toggle()
-	-- 			end,
-	-- 			desc = "Toggle Relative [L]ine [N]umbers",
-	-- 		},
-	-- 		{
-	-- 			"<leader>cl",
-	-- 			function()
-	-- 				Snacks.toggle.option("cursorline", { name = "Cursor Line" }):toggle()
-	-- 			end,
-	-- 			desc = "Toggle [C]ursor [L]ine",
-	-- 		},
-	-- 		{
-	-- 			"<leader>td",
-	-- 			function()
-	-- 				Snacks.toggle.diagnostics():toggle()
-	-- 			end,
-	-- 			desc = "[T]oggle [D]iagnostics",
-	-- 		},
-	-- 		{
-	-- 			"<leader>tt",
-	-- 			function()
-	-- 				local tsc = require("treesitter-context")
-	-- 				Snacks.toggle({
-	-- 					name = "Treesitter Context",
-	-- 					get = tsc.enabled,
-	-- 					set = function(state)
-	-- 						if state then
-	-- 							tsc.enable()
-	-- 						else
-	-- 							tsc.disable()
-	-- 						end
-	-- 					end,
-	-- 				}):toggle()
-	-- 			end,
-	-- 			desc = "[T]oggle [T]reesitter Context",
-	-- 		},
-	-- 	},
-	-- },
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			bigfile = { enabled = true },
+			dim = { enabled = true },
+			git = { enabled = true },
+			gitbrowse = { enabled = true },
+			indent = { enabled = true },
+			notifier = {
+				enabled = true,
+				timeout = 3000,
+			},
+			quickfile = { enabled = true },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+			zen = { enabled = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "VeryLazy",
+				callback = function()
+					local notify = Snacks.notifier.notify
+					Snacks.notifier.notify = function(message, level, opts)
+						for _, msg in ipairs(filtered_message) do
+							if message == msg then
+								return nil
+							end
+						end
+						return notify(message, level, opts)
+					end
+				end,
+			})
+		end,
+		keys = {
+			{
+				"<leader>.",
+				function()
+					Snacks.scratch()
+				end,
+				desc = "Toggle Scratch Buffer",
+			},
+			{
+				"<leader>B",
+				function()
+					Snacks.scratch.select()
+				end,
+				desc = "Select Scratch [B]uffer",
+			},
+			{
+				"<leader>bd",
+				function()
+					Snacks.bufdelete()
+				end,
+				desc = "[B]uffer [D]elete",
+			},
+			{
+				"<leader>gb",
+				function()
+					Snacks.git.blame_line()
+				end,
+				desc = "[G]it [B]lame Line",
+			},
+			{
+				"<leader>og",
+				function()
+					Snacks.gitbrowse()
+				end,
+				desc = "[O]pen [G]it",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>nh",
+				function()
+					Snacks.notifier.show_history()
+				end,
+				desc = "Notification History",
+			},
+			{
+				"<leader>dn",
+				function()
+					Snacks.notifier.hide()
+				end,
+				desc = "[D]ismiss All [N]otifications",
+			},
+			{
+				"<leader>nh",
+				function()
+					Snacks.notifier.show_history()
+				end,
+				desc = "[N]otification [H]istory",
+			},
+			{
+				"<leader>z",
+				function()
+					Snacks.toggle.dim():toggle()
+				end,
+				desc = "Toggle [Z]en Mode",
+			},
+
+			{
+				"<leader>ln",
+				function()
+					Snacks.toggle.option("relativenumber", { name = "Relative Number" }):toggle()
+				end,
+				desc = "Toggle Relative [L]ine [N]umbers",
+			},
+			{
+				"<leader>cl",
+				function()
+					Snacks.toggle.option("cursorline", { name = "Cursor Line" }):toggle()
+				end,
+				desc = "Toggle [C]ursor [L]ine",
+			},
+			{
+				"<leader>td",
+				function()
+					Snacks.toggle.diagnostics():toggle()
+				end,
+				desc = "[T]oggle [D]iagnostics",
+			},
+			{
+				"<leader>tt",
+				function()
+					local tsc = require("treesitter-context")
+					Snacks.toggle({
+						name = "Treesitter Context",
+						get = tsc.enabled,
+						set = function(state)
+							if state then
+								tsc.enable()
+							else
+								tsc.disable()
+							end
+						end,
+					}):toggle()
+				end,
+				desc = "[T]oggle [T]reesitter Context",
+			},
+		},
+	},
 }
