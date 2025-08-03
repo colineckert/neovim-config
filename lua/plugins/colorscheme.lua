c78d1385114c159b4f82d8f7f432646987de0c2f
Colin Eckert <colineckert10@gmail.com>
2025-08-03 08:48:02 -0700

    cleanup colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,9 +4,6 @@ return {
 		lazy = false,
 		priority = 1000,
 		opts = {
-			---@class tokyonight.Config
-			---@field on_colors fun(colors: ColorScheme)
-			---@field on_highlights fun(highlights: tokyonight.Highlights, colors: ColorScheme)
 			style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
 			light_style = "day", -- The theme is used when the background is set to light
 			transparent = false, -- Enable this to disable setting the background color
@@ -54,69 +51,69 @@ return {
 			},
 		},
 	},
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "auto", -- latte, frappe, macchiato, mocha
-				background = { -- :h background
-					light = "latte",
-					dark = "mocha",
-				},
-				transparent_background = false, -- disables setting the background color.
-				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-				term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-				dim_inactive = {
-					enabled = false, -- dims the background color of inactive window
-					shade = "dark",
-					percentage = 0.15, -- percentage of the shade to apply to the inactive window
-				},
-				no_italic = false, -- Force no italic
-				no_bold = false, -- Force no bold
-				no_underline = false, -- Force no underline
-				-- styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-				-- 	comments = { "italic" }, -- Change the style of comments
-				-- 	conditionals = { "italic" },
-				-- 	loops = {},
-				-- 	functions = { "italic" },
-				-- 	keywords = {},
-				-- 	strings = {},
-				-- 	variables = {},
-				-- 	numbers = {},
-				-- 	booleans = {},
-				-- 	properties = {},
-				-- 	types = {},
-				-- 	operators = {},
-				-- miscs = {}, -- Uncomment to turn off hard-coded styles
-				-- },
-				color_overrides = {},
-				custom_highlights = {},
-				default_integrations = true,
-				integrations = {
-					treesitter = true,
-					cmp = true,
-					gitsigns = true,
-					markdown = true,
-					nvimtree = false,
-					notify = false,
-					diffview = true,
-					fidget = true,
-					harpoon = true,
-					mason = true,
-					-- indent_blankline = {
-					-- 	enabled = false,
-					-- 	scope_color = "sapphire",
-					-- 	colored_indent_levels = false,
-					-- },
-					mini = {
-						enabled = true,
-						indentscope_color = "sapphire",
-					},
-				},
-			})
-		end,
-	},
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	lazy = false,
+	-- 	name = "catppuccin",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("catppuccin").setup({
+	-- 			flavour = "auto", -- latte, frappe, macchiato, mocha
+	-- 			background = { -- :h background
+	-- 				light = "latte",
+	-- 				dark = "mocha",
+	-- 			},
+	-- 			transparent_background = false, -- disables setting the background color.
+	-- 			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+	-- 			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+	-- 			dim_inactive = {
+	-- 				enabled = false, -- dims the background color of inactive window
+	-- 				shade = "dark",
+	-- 				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+	-- 			},
+	-- 			no_italic = false, -- Force no italic
+	-- 			no_bold = false, -- Force no bold
+	-- 			no_underline = false, -- Force no underline
+	-- 			-- styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+	-- 			-- 	comments = { "italic" }, -- Change the style of comments
+	-- 			-- 	conditionals = { "italic" },
+	-- 			-- 	loops = {},
+	-- 			-- 	functions = { "italic" },
+	-- 			-- 	keywords = {},
+	-- 			-- 	strings = {},
+	-- 			-- 	variables = {},
+	-- 			-- 	numbers = {},
+	-- 			-- 	booleans = {},
+	-- 			-- 	properties = {},
+	-- 			-- 	types = {},
+	-- 			-- 	operators = {},
+	-- 			-- miscs = {}, -- Uncomment to turn off hard-coded styles
+	-- 			-- },
+	-- 			color_overrides = {},
+	-- 			custom_highlights = {},
+	-- 			default_integrations = true,
+	-- 			integrations = {
+	-- 				treesitter = true,
+	-- 				cmp = true,
+	-- 				gitsigns = true,
+	-- 				markdown = true,
+	-- 				nvimtree = false,
+	-- 				notify = false,
+	-- 				diffview = true,
+	-- 				fidget = true,
+	-- 				harpoon = true,
+	-- 				mason = true,
+	-- 				-- indent_blankline = {
+	-- 				-- 	enabled = false,
+	-- 				-- 	scope_color = "sapphire",
+	-- 				-- 	colored_indent_levels = false,
+	-- 				-- },
+	-- 				mini = {
+	-- 					enabled = true,
+	-- 					indentscope_color = "sapphire",
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 }
